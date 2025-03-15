4f54e2d4a559c72a93e04e2591cdf779f1f255fe
lBenekin <b3n3kin@gmail.com>
2025-03-15 21:51:27 +0100

    Added basic statistics and put requests are needs to be done

--- BACKEND/ExamServer/ExamServer/Data/StudentRepository.cs
+++ BACKEND/ExamServer/ExamServer/Data/StudentRepository.cs
@@ -10,11 +10,13 @@ namespace ExamServer.Data
 
         Student GetById(int id);
 
+        Statistic GetStatistics(int id);
         void Add(Student entity);
 
         void Update(Student entity);
 
         void Delete(int id);
+
     }
     public class StudentRepository : IStudentRepository
     {
@@ -37,7 +39,6 @@ namespace ExamServer.Data
                 .ThenInclude(g => g.Subject)
                 .FirstOrDefault(s => s.Id == id);
         }
-
         public void Add(Student student)
         {
             _context.Students.Add(student);
@@ -59,5 +60,57 @@ namespace ExamServer.Data
                 _context.SaveChanges();
             }
         }
+        public Statistic GetStatistics(int id)
+        {
+            var student = GetById(id);
+            if (student == null)
+                return null;
+            return new Statistic
+            {
+                Average = GetAverage(student),
+                Median = GetMedian(student),
+                Mode = GetMode(student),
+                Distribution = GetDistribution(student)
+            };
+        }
+
+        private double GetDistribution(Student student)
+        {
+            if (student == null)
+                return 0;
+            var grades = student.Grades.Select(g => g.GradeValue).ToList();
+            var max = grades.Max();
+            var min = grades.Min();
+            return max - min;
+        }
+
+        private double GetMode(Student student)
+        {
+            if (student == null)
+                return 0;
+            var grades = student.Grades.Select(g => g.GradeValue).ToList();
+            var mode = grades.GroupBy(g => g)
+                .OrderByDescending(g => g.Count())
+                .Select(g => g.Key)
+                .FirstOrDefault();
+            return mode;
+        }
+
+        private double GetMedian(Student student)
+        {
+            if (student == null)
+                return 0;
+            var grades = student.Grades.Select(g => g.GradeValue).OrderBy(g => g).ToList();
+            var count = grades.Count;
+            if (count % 2 == 0)
+                return (grades[count / 2 - 1] + grades[count / 2]) / 2;
+            return grades[count / 2];
+        }
+
+        private double GetAverage(Student student)
+        {
+            return student == null ? 0 : student.Grades.Average(g => g.GradeValue);
+        }
+        
     }
 }
